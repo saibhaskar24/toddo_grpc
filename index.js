1d49b81f4cd645b09fd65d35901f80fb128a0508
saibhaskar24 <saibaaskar24091999@gmail.com>
2020-07-28 20:42:47 +0530

    Change

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const database = require("./database");
+
 const grpc = require("grpc");
+
+
+
 const protoLoader = require("@grpc/proto-loader")
 const packageDef = protoLoader.loadSync("todo.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
 const todoPackage = grpcObject.todoPackage;
+
 
 const server = new grpc.Server();
 server.bind("0.0.0.0:40000",grpc.ServerCredentials.createInsecure());
@@ -18,6 +24,7 @@ server.addService(todoPackage.Todo.service,
         "findTodo":findTodo,
     });
 server.start();
+
 
 const todos = []
 function createTodo (call, callback) {
